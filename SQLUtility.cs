af924d893deaf1911cb038de8e4a234ed7c9f400
Herskovitz <hi31985508@gmail.com>
2024-09-08 15:56:03 -0400

    initial commit

---
+++ SQLUtility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPUFramework
+{
+    public class SQLUtility
+    {
+        public static string ConnectionString = "";
+        public static DataTable GetDataTable(string sqlstatement) // - take a SQL statement and return DataTable
+        {
+
+            Debug.Print(sqlstatement);
+            DataTable dt = new();
+            SqlConnection conn = new();
+            conn.ConnectionString = ConnectionString;
+            conn.Open();
+            //DisplayMessage("Conn Status",conn.State.ToString());
+            var cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = sqlstatement;
+            var dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            return dt;
+        }
+
+    }
+}
